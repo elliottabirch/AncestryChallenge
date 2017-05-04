f2e96f7da9b4e8ffee20528de83d80ad406cd631
elliottabirch <elliottabirch@gmail.com>
2017-05-04 14:56:16 -0700

    resytle collectionPage

--- src/App/CollectionPage/SideBlock/index.jsx
+++ src/App/CollectionPage/SideBlock/index.jsx
@@ -5,19 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="sideBlockContainer">
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-        <div className="sideBlock">helloaa sdfasdf asdfasdfa sdfasdfasdfasfasd fasdfsdfas fsdfasdf asdfasfasd fasdfas</div>
-
+        {this.props.children}
       </div>
     );
   }

c6e458bbf9230e251480a41dceb615d2f69d834d
elliottabirch <elliottabirch@gmail.com>
2017-05-04 14:56:29 -0700

    move Footer

---
+++ src/App/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+
+function Footer(props) {
+  return (
+      <div className="footer" >
+        <div>1997-2015 Ancestry</div>
+        <a>Corporate Information</a>
+        <a>Privacy</a>
+        <a>Terms and Conditions</a>
+        <a>Site Map</a>
+      </div>
+    );
+}
+
+
+export default Footer;

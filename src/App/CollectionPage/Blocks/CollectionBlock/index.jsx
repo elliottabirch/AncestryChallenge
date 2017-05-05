e242e5a84ad0a7302aabc8630fbdd2b338b18c81
elliottabirch <elliottabirch@gmail.com>
2017-05-05 14:05:55 -0700

    style collectionblock

--- src/App/CollectionPage/Blocks/CollectionBlock/index.jsx
+++ src/App/CollectionPage/Blocks/CollectionBlock/index.jsx
@@ -3,13 +3,13 @@ import './CollectionBlock.css';
 
 
 const CollectionBlock = (props) => {
-  const collection = (props.collection && props.collection.map((item, index) => <li key={`${item}${index}`}><a>{item}</a></li>));
+  const collection = (props.collection && props.collection.map((item, index) => <div className="collectionItem" key={`${item}${index}`}><a>{item}</a></div>));
 
   return (
-    <div>
-      <h2>{props.title && props.title}</h2>
-      <ul>{collection}</ul>
-      <button>SEE ALL COLLECTIONS</button>
+    <div className="collectionBlock">
+      <div className="collectionBlockTitle">{props.title && props.title}</div>
+      {collection}
+      <button className="seeCollectionsButton">SEE ALL COLLECTIONS</button>
     </div>
   );
 };
